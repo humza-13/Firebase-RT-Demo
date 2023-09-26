e7d712301f5ffdf2e652f8d6f58aaa0037bf1d61
Muhammad Humza Butt <leonhumza@gmail.com>
2023-09-26 17:19:30 +0500

    Game Start Event

--- Assets/_Main/Scripts/DB/RTManager.cs
+++ Assets/_Main/Scripts/DB/RTManager.cs
@@ -1,13 +1,16 @@
-﻿using Firebase;
+﻿using System;
+using Firebase;
 using Firebase.Database;
 using Phoenix.Firebase.Managers;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Phoenix.Firebase.RT
 {
     public class RTManager : FirebaseManager
     {
         public static RTManager _rtInstance;
+        public string CUURENT_SESSION;
         
         #region Overrides
         protected override void Start()
@@ -36,15 +39,15 @@ namespace Phoenix.Firebase.RT
         #region Hosting
         public void HostGame(string playerUid)
         {
-            string sessionId = GenerateUniqueSessionId();
-            
+            CUURENT_SESSION = GenerateUniqueSessionId();
             var sessionData = new GameSession()
             {
                 player1_uid = playerUid,
                 player2_uid = "",
                 game_started = false
             };
-            databaseReference.Child(sessionId).SetRawJsonValueAsync(JsonUtility.ToJson(sessionData));
+            databaseReference.Child(CUURENT_SESSION).SetRawJsonValueAsync(JsonUtility.ToJson(sessionData));
+            SetGameStartEvent();
         }
         private string GenerateUniqueSessionId()
         {
@@ -61,7 +64,7 @@ namespace Phoenix.Firebase.RT
             {
                 foreach (var session in snapshot.Children)
                 {
-                    string sessionId = session.Key;
+                    CUURENT_SESSION = session.Key;
                     var sessionData = session.GetRawJsonValue();
                     GameSession gameSession = JsonUtility.FromJson<GameSession>(sessionData);
                     
@@ -70,16 +73,47 @@ namespace Phoenix.Firebase.RT
                         gameSession.player2_uid = currentPlayerUid;
                         gameSession.game_started = true;
                         
-                        await databaseReference.Child(sessionId).SetRawJsonValueAsync(JsonUtility.ToJson(gameSession));
-                        Debug.Log("Joined game session: " + sessionId);
+                        await databaseReference.Child(CUURENT_SESSION).SetRawJsonValueAsync(JsonUtility.ToJson(gameSession));
+                        SetGameStartEvent();
                         return;
                     }
                 }
             }
-            Debug.Log("No available game sessions found. Creating one!");
             HostGame(currentPlayerUid);
         }
 
+        #endregion
+        
+        #region Events
+        private void SetGameStartEvent()
+        {
+            DatabaseReference db = databaseReference
+                .Child(CUURENT_SESSION).Child("game_started");
+            
+            db.ValueChanged += (sender, args) =>
+            {
+                if (args.DatabaseError != null)
+                {
+                    Debug.LogError("Error reading game_started: " + args.DatabaseError.Message);
+                    return;
+                }
+
+                bool gameStarted = Convert.ToBoolean(args.Snapshot.Value);
+    
+                if (gameStarted)
+                    Debug.Log("Game has started!");
+            };
+        }
+
+        #endregion
+
+        #region Helper
+
+        public DatabaseReference GetDBReference()
+        {
+            return databaseReference;
+        }
+        
         #endregion
         
     }
--- Assets/_Main/Scripts/Network/NetworkManager.cs
+++ Assets/_Main/Scripts/Network/NetworkManager.cs
@@ -1,17 +1,20 @@
-﻿using Phoenix.Firebase.Auth;
+﻿using System;
+using Firebase.Database;
+using Phoenix.Firebase.Auth;
 using Phoenix.Firebase.RT;
 using UnityEngine;
 
 namespace Phoenix.Network
 {
-   // [RequireComponent(typeof(AuthManager))]
-   // [RequireComponent(typeof(RTManager))]
     public class NetworkManager : MonoBehaviour
     {
+        #region Match Making
         public void HostGame() => 
             RTManager._rtInstance.HostGame(AuthManager._authInstance.GetCurrentUser().UserId);
         public void JoinGame() => 
             RTManager._rtInstance.JoinGame(AuthManager._authInstance.GetCurrentUser().UserId);
+        #endregion
         
+
     }
 }
